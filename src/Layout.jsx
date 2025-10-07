9550e252ddbe1d4d062de5d8f1c0ee86b1902167
Noviansyah <cymbalavedis@gmail.com>
2025-10-07 22:00:43 +0700

    fourth commit

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -4,13 +4,13 @@ import Navbar from "./components/Navbar";
 export default function Layout() {
   return (
     <>
-      <div className="w-full h-screen fixed bg-[url(src/assets/bg.jpg)] bg-cover bg-center bg-no-repeat"></div>
+      <div className="w-full h-screen fixed bg-[url(/bg.jpg)] bg-cover bg-center bg-no-repeat"></div>
       <div className="w-full m-auto lg:w-4/5 xl:w-5/6 lg:py-7 relative z-50">
-        <div className="top-0 right-0 left-0 fixed mt-2">
+        <div className="top-0 right-0 left-0 fixed">
           <Navbar />
         </div>
 
-        <div className="w-full px-2 mt-18 mb-14 md:mt-24">
+        <div className="w-full px-2 mt-16 mb-14 md:mt-24">
           <Outlet />
         </div>
 
