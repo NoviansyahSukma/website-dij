cc2601bf7d72260c301205b960bcc2ea7e909fc3
Noviansyah <cymbalavedis@gmail.com>
2025-10-07 21:36:26 +0700

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import "./main.css";
 import Home from "./pages/Home";
 import Layout from "./Layout";
@@ -8,13 +8,13 @@ import About from "./pages/About";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           {/* <Route path="about" element={<About />} /> */}
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
